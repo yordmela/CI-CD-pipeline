7a25eeb3e3bc160797da73c9c915ebc25f2e50bc
Yordanos <yordanos.mela@gmail.com>
2025-03-21 20:09:26 +0300

    fix: linter again

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 def fizzbuzz(n):
-    if n%3==0 and n%5==0:
+    if n % 3 == 0 and n % 5 == 0:
         return "FizzBuzz"
-    elif n%3==0:
+    elif n % 3 == 0:
         return "Fizz"
-    elif n%5==0:
+    elif n % 5 == 0:
         return "Buzz"
     else:
         return str(n)
 
 
 def main():
-    n= int(input("Enter a number: "))
+    n = int(input("Enter a number: "))
     print(fizzbuzz(n))
 
+
 if __name__ == "__main__":
-   main()
+    main()
