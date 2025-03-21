81200a3752943baaaa973eafa9631d774c842201
Yordanos <yordanos.mela@gmail.com>
2025-03-21 20:12:30 +0300

    fix: linter final

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,7 +1,11 @@
 from main import fizzbuzz
 
+
+# Add an extra blank line here
 def test_fizzbuzz():
     assert fizzbuzz(3) == "Fizz"
     assert fizzbuzz(5) == "Buzz"
     assert fizzbuzz(15) == "FizzBuzz"
     assert fizzbuzz(2) == "2"
+
+
